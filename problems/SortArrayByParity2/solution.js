5d3e660ed996d568bb3d437d771635158508f418
Vaughn Hass <vaughnhass@gmail.com>
2021-09-29 00:28:47 +0000

    finished SortArrayByParity in 2 different ways, one being as an in-place algorithm

---
+++ problems/SortArrayByParity2/solution.js
@@ -0,0 +1,100 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ 
+ /*
+ Solution ideas:
+    - create 2 arrays & sort num into them, one holds even & one holds odds. Then combine.
+        - comp: o(2n), storage: o(3n)
+        
+    - do it in place:
+        - start at i = 0 (loop)
+            - if 0 is even, move to 1. if 1 is odd, move to 2...
+            - when you get to one that is out of place: 
+                - store the index of it
+                - then move to next index & see if it is the proper parity (loop)
+                    - if it isnt, continue on
+                    - if it is, swap it with the element at the stored index
+                    - exit loop
+            - once a swap is made, continue main loop from index stored + 1
+            - exit loop when i = nums.length - 1
+        - return nums
+ */
+var sortArrayByParityII_Easy = function(nums) {
+    let even = [];
+    let odd = [];
+    let out = [];
+    
+    
+    nums.forEach((num) => {
+        num%2 === 0 ? even.push(num) : odd.push(num);
+    });
+     
+    while(even != false || odd != false){
+        if(even != false) out.push(even.pop());
+        if(odd != false) out.push(odd.pop());
+    }
+    
+    return out;
+};
+
+function searchFromIndex(i, arr, evenOrOdd){
+    /*
+    searches through arr starting at i+1, in the + direction
+    
+    returns index of first value that:
+      - matches req of evenOrOdd (true: even,...)
+      - is also in the wrong index Parity-wise
+    */
+    let found = false;
+    let idx = i + 1;
+    
+    while(idx < arr.length && !found){
+        
+        if(evenOrOdd && idx % 2 === 1 && arr[idx] % 2 === 0){
+            found = true;
+            return idx;
+        }
+        if(!evenOrOdd && idx % 2 === 0 && arr[idx] % 2 === 1){
+            found = true;
+            return idx;
+        }
+        
+        idx++;
+    }
+}
+
+var sortArrayByParityII_InPlace = function(nums) {
+    for(let i = 0; i < nums.length; i++){
+        
+        if(i % 2 === 0 && nums[i] % 2 === 1){ //even action condition
+            //find index of element to swap
+            let indexToSwap = searchFromIndex(i, nums, true);
+            //do swap
+            [nums[i], nums[indexToSwap]] = [nums[indexToSwap], nums[i]];
+            
+            
+        } else if(i % 2 === 1 && nums[i] % 2 === 0) { //odd action condition
+            //find index of element to swap
+            let indexToSwap = searchFromIndex(i, nums, false);
+            //do swap
+            [nums[i], nums[indexToSwap]] = [nums[indexToSwap], nums[i]];
+            
+        }
+    }
+    
+    return nums;
+};
+
+function driver(){
+    let input = [4,2,5,7];
+    let output = [];
+    
+    //output = sortArrayByParityII_Easy(input);
+    output = sortArrayByParityII_InPlace(input);
+    
+    console.log(output);
+}
+
+driver();
